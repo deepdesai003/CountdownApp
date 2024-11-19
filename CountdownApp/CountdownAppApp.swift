0005de4491cd19449f5a0842c6db55ef2cb5106a
Deep Desai <ddesai@kodebright.com>
2024-11-18 21:49:53 -0500

    Initial Commit

---
+++ CountdownApp/CountdownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownAppApp.swift
+//  CountdownApp
+//
+//  Created by Deep on 2024-11-18.
+//
+
+import SwiftUI
+
+@main
+struct CountdownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
